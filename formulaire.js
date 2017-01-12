a0cee405e617b9c0a441b1cb91e899e838d285ea
Denis <denis.declercq@texa.fr>
2017-01-12 15:00:49 +0100

    Add Email field

--- formulaire.js
+++ formulaire.js
@@ -14,6 +14,10 @@ var FormulaireSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  email: {
+    type: String,
+    required: true
+  },
   modifiedTimestamp: {
     type: Number
   },
